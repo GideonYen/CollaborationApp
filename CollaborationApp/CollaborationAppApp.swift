3c18076d4c6e4b96f320d4417ca55f595f525319
Gideon Yen <gideon.yen20@gmail.com>
2023-05-31 13:36:11 +0800

    Initial Commit

---
+++ CollaborationApp/CollaborationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollaborationAppApp.swift
+//  CollaborationApp
+//
+//  Created by Sapphire Blue on 31/5/23.
+//
+
+import SwiftUI
+
+@main
+struct CollaborationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
