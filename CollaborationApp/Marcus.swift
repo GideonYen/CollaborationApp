d7999f74ce4d61ec8acddf718cc4ef5a02babc89
Nothing <106498416+AlphaSolid@users.noreply.github.com>
2023-05-31 15:05:23 +0800

    Added new view  or smth

--- CollaborationApp/Marcus.swift
+++ CollaborationApp/Marcus.swift
@@ -5,4 +5,27 @@
 //  Created by Milind Contractor on 31/5/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct MarcsView: View {
+    var body: some View {
+        ZStack {
+            LinearGradient(colors: [.red, .yellow],
+            startPoint: .topTrailing,
+            endPoint: .bottomTrailing)
+            .ignoresSafeArea()
+            
+            VStack {
+                Text("Marcus")
+                    .font(.largeTitle)
+                
+            }
+        }
+    }
+}
+
+struct MarcsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
